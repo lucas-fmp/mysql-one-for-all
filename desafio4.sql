81759d881d42d6fd2083b3f279be718aedfcef0c
Lucas Fernando <lucasfernandomacedo13@gmail.com>
2022-09-26 18:10:44 -0300

    requirement 04

---
+++ desafio4.sql
@@ -0,0 +1,11 @@
+SELECT
+	u.full_name AS usuario,
+    IF(YEAR(MAX(h.date)) >= 2021,
+		'Usuário ativo',
+        'Usuário inativo') AS status_usuario
+FROM
+	SpotifyClone.users AS u
+INNER JOIN
+	SpotifyClone.history AS h ON u.user_id = h.user_id
+GROUP BY u.full_name
+ORDER BY u.full_name;
