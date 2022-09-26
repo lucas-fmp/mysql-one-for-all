b9e7e4db0ab4ab4864a61ff80cbde8610d28234c
Lucas Fernando <lucasfernandomacedo13@gmail.com>
2022-09-26 18:17:54 -0300

    requirement 05

---
+++ desafio5.sql
@@ -0,0 +1,10 @@
+SELECT
+	s.name AS cancao,
+    COUNT(h.song_id) as reproducoes
+FROM
+	SpotifyClone.songs AS s
+INNER JOIN
+	SpotifyClone.history AS h ON s.song_id = h.song_id
+GROUP BY cancao
+ORDER BY reproducoes DESC, cancao
+LIMIT 2;
