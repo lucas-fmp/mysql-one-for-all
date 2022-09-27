c94a63e5038331ddd19a92f3e1f9e58e356c4340
Lucas <lucasfernandomacedo13@gmail.com>
2022-09-27 08:59:13 -0300

    requirement 08

---
+++ desafio8.sql
@@ -0,0 +1,11 @@
+SELECT
+	art.full_name AS artista,
+    alb.album AS album
+FROM
+	SpotifyClone.artists AS art
+INNER JOIN
+	SpotifyClone.albuns AS alb ON art.artist_id = alb.artist_id
+WHERE
+	art.full_name = 'Elis Regina'
+GROUP BY artista, album
+ORDER BY album;
